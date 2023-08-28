2a916ec9793b6cfdfa267cb8f0c277927c0d4ade
tichymi5 <michal.tichy.y@email.cz>
2023-08-28 23:12:57 +0200

    Added script for generating ASTs

---
+++ GenerateAst/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+
+namespace GenerateAst
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: generate_ast <output directory>");
+                return 64;
+            }
+
+            var outputDir = args[0];
+            var exprOutput = DefineAst("Expr", new()
+            {
+                ("BinaryExpr", new() { ("Expr", "Left"), ("Expr", "Right"), ("Token", "Op") }),
+                ("UnaryExpr", new() { ("Expr", "Right"), ("Token", "Op") }),
+                ("LiteralExpr", new() { ("object", "Value") }),
+                ("GroupingExpr", new() { ("Expr", "Expression") }),
+            });
+            var exprFile = outputDir + "/" + "Expr.cs";
+            File.WriteAllText(exprFile, exprOutput);
+
+            return 0;
+        }
+
+        static string DefineAst(
+            string baseClass,
+            List<(string className, List<(string type, string name)> properties)> subclasses,
+            string namespaceName = "LoxInterpreter"
+        )
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"namespace {namespaceName}");
+            stringBuilder.AppendLine("{");
+
+            stringBuilder.AppendLine(defineVisitor(baseClass, subclasses.Select(subclass => subclass.className)));
+
+            stringBuilder.AppendLine($"  internal abstract class {baseClass}");
+            stringBuilder.AppendLine("  {");
+            stringBuilder.AppendLine($"  public abstract T Accept<T>(I{baseClass}Visitor<T> visitor);");
+            stringBuilder.AppendLine("  }");
+            
+            foreach (var (className, properties) in subclasses)
+            {
+                stringBuilder.AppendLine($"  internal class {className} : {baseClass}");
+                stringBuilder.AppendLine("  {");
+                // Properties
+                foreach (var property in properties)
+                {
+                    stringBuilder.AppendLine($"      public {property.type} {property.name} {{ get; }}");
+                }
+                // Constructor
+                var parameters = string.Join(", ", properties.Select(property => $"{property.type} {property.name.ToLower()}"));
+                stringBuilder.AppendLine($"      public {className}({parameters})");
+                stringBuilder.AppendLine("      {");
+                foreach (var property in properties)
+                {
+                    stringBuilder.AppendLine($"         {property.name} = {property.name.ToLower()};");
+                }
+                stringBuilder.AppendLine("      }");
+                // Accept
+                stringBuilder.AppendLine($"     public override T Accept<T>(I{baseClass}Visitor<T> visitor)");
+                stringBuilder.AppendLine("      {");
+                stringBuilder.AppendLine("          return visitor.Visit(this);");
+                stringBuilder.AppendLine("      }");
+
+                stringBuilder.AppendLine("  }");
+            }
+            stringBuilder.AppendLine("}");
+
+            return stringBuilder.ToString();
+        }
+
+        private static string defineVisitor(string baseClass, IEnumerable<string> classes)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"  internal interface I{baseClass}Visitor<T>");
+            stringBuilder.AppendLine("  {");
+            foreach (var className in classes)
+            {
+                stringBuilder.AppendLine($"     T Visit({className} visitee);");
+            }
+            stringBuilder.AppendLine("  }");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ LoxInterpreter/Expr.cs
@@ -0,0 +1,69 @@
+namespace LoxInterpreter
+{
+  internal interface IExprVisitor<T>
+  {
+     T Visit(BinaryExpr visitee);
+     T Visit(UnaryExpr visitee);
+     T Visit(LiteralExpr visitee);
+     T Visit(GroupingExpr visitee);
+  }
+
+  internal abstract class Expr
+  {
+  public abstract T Accept<T>(IExprVisitor<T> visitor);
+  }
+  internal class BinaryExpr : Expr
+  {
+      public Expr Left { get; }
+      public Expr Right { get; }
+      public Token Op { get; }
+      public BinaryExpr(Expr left, Expr right, Token op)
+      {
+         Left = left;
+         Right = right;
+         Op = op;
+      }
+     public override T Accept<T>(IExprVisitor<T> visitor)
+      {
+          return visitor.Visit(this);
+      }
+  }
+  internal class UnaryExpr : Expr
+  {
+      public Expr Right { get; }
+      public Token Op { get; }
+      public UnaryExpr(Expr right, Token op)
+      {
+         Right = right;
+         Op = op;
+      }
+     public override T Accept<T>(IExprVisitor<T> visitor)
+      {
+          return visitor.Visit(this);
+      }
+  }
+  internal class LiteralExpr : Expr
+  {
+      public object Value { get; }
+      public LiteralExpr(object value)
+      {
+         Value = value;
+      }
+     public override T Accept<T>(IExprVisitor<T> visitor)
+      {
+          return visitor.Visit(this);
+      }
+  }
+  internal class GroupingExpr : Expr
+  {
+      public Expr Expression { get; }
+      public GroupingExpr(Expr expression)
+      {
+         Expression = expression;
+      }
+     public override T Accept<T>(IExprVisitor<T> visitor)
+      {
+          return visitor.Visit(this);
+      }
+  }
+}
